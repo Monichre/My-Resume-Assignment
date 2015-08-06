368cee2e4f990c76c0a1b8d24968587a339ccc31
Liam <liamhellis@gmail.com>
2015-08-06 13:53:15 -0400

    formatting single page scroll layout
    Going better than I thought

--- js/index.js
+++ js/index.js
@@ -9,36 +9,9 @@
   //   $('a').fadeIn(1000);
 
   // });
-$(document).ready( function() {
-    var slider = $.fn.fsvs({
-        speed : 5000,
-        bodyID : 'fsvs-body',
-        selector : '> .slide',
-        mouseSwipeDisance : 40,
-        afterSlide : function(){},
-        beforeSlide : function(){},
-        endSlide : function(){},
-        mouseWheelEvents : true,
-        mouseWheelDelay : false,
-        scrollableArea : 'scrollable',
-        mouseDragEvents : true,
-        touchEvents : true,
-        arrowKeyEvents : true,
-        pagination : true,
-        nthClasses : false,
-        detectHash : true
-    });
-  
-  var slider = $.fn.fsvs({
-    ...
-    endSlide : function(index) {
-      $('<div class="slide"><h2>Slide ' + (index+2) + '</h2></div>').appendTo( $('body') );
-      // reset the nth classes if you need it
-      slider.nthClasses(3);
-      // rebind pagination if you need it
-      slider.addPagination();
-    }
-    ...
+$(function() {
+  $.scrollify({
+    section : "section",
   });
         
 });
