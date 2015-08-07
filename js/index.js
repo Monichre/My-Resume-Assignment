0ddf9108775163b1d9c81a63bb6064b0c190bc95
Liam <liamhellis@gmail.com>
2015-08-06 22:13:31 -0400

    More changes
    getting rid of resume scroll page
    
    finalizing home page
    
    tinkering with masonry scroll page

--- js/index.js
+++ js/index.js
@@ -1,17 +1,42 @@
 
 
-  // $('.img-circle').fadeIn(1000);
-
-  // $('#email').hide();
-
-  // $('.img-circle').click(function(){
-
-  //   $('a').fadeIn(1000);
-
-  // });
+  
 $(function() {
+
+//---This is my scrollify Jquery plug-in---//
+
   $.scrollify({
     section : "section",
   });
+
+//---This is my custom Jquery---//
+
+  $('.img-circle').fadeIn(1000);
+
+  $('#email').hide();
+
+  $('.img-circle').click(function(){
+
+    $('a').fadeIn(1000);
+
+  });
+
+  //-- This is my masonry Jquery plug-in--//
+
+  $('.grid').masonry({
+  "itemSelector": '.grid-item',
+  "gutter": 10,
+  "isFitWidth": true
+});
+
+  //---this is my jcloud attempt---//
+
+  $('#sky').jQlouds({
+  wind: true,
+  minClouds: 5,
+  maxClouds: 10
+});
+
+
         
 });
